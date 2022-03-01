f767eca7caa3eebea0895eab1ea86c96e3fb7c98
HarishKunchalwar <harishkunchalwar@gmail.com>
2022-03-01 16:11:57 +0530

    Function headers removed

--- math.py
+++ math.py
@@ -1,20 +1,23 @@
-#addition 
+
 def add(x,y):
 	pass
 
-#Subtraction
+
 def sub(x,y):
 	return x-y
 
-#Multiplication
+
 def mul(x,y):
 	return x*y 
-#Division
+
+
+
 def div(x,y):			#On Master Branch
 	if y==0:
 		return DIVIDE_BY_0_ERROR
 	else :
 		return x/y
-#Squre function
+
+
 def square(x):
 	return x*x
